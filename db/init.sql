a2bd742a5e6b685a6defa6a1f6c9b0a047c13419
nadiika09 <nadiika09@ukr.net>
2026-04-11 03:12:25 +0800

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -3,12 +3,9 @@ CREATE TABLE services (
     name VARCHAR(100),
     price DECIMAL(10, 2)
 );
-
 CREATE TABLE masters (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     specialty VARCHAR(100)
 );
-
 INSERT INTO services (name, price) VALUES ('Haircut', 500), ('Manicure', 800);
-INSERT INTO masters (name, specialty) VALUES ('Anna', 'Stylist'), ('Oleg', 'Barber');
